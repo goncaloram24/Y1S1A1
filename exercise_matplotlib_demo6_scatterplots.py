85a7198768337fcff1003bbfe00a735c19fd8d4d
Gonçalo Moreira <goncaloram@hotmail.com>
2022-09-14 18:28:28 +0100

    Demo 6 related to matplotlib, scatterplots is now, truly complete

--- exercise_matplotlib_demo6_scatterplots.py
+++ exercise_matplotlib_demo6_scatterplots.py
@@ -5,6 +5,7 @@ friends = np.array([70, 65, 72, 63, 71, 64, 60, 64, 67])
 minutes = np.array([175, 170, 205, 120, 220, 130, 105, 145, 190])
 labels  = np.array(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'])
 
+plt.figure(1)
 plt.scatter(friends, minutes)
 
 # label each point
@@ -17,4 +18,14 @@ for label, friend_count, minute_count in zip(labels, friends, minutes):
 plt.title("Daily Minutes vs Number of Friends")
 plt.xlabel("# of friends")
 plt.ylabel("daily minutes spent on the site")
+
+plt.figure(2)
+test_1_grades = np.array([99, 90, 85, 97, 80])
+test_2_grades = np.array([100, 85, 60, 90, 70])
+
+plt.scatter(test_1_grades, test_2_grades)
+plt.title("Axes aren't comparable")
+plt.xlabel("Test 1 Grades")
+plt.ylabel("Test 2 Grades")
+
 plt.show()
