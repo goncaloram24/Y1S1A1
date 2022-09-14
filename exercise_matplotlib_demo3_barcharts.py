10abeefe3400eede7b2584697de45312c0c8a0a1
Gonçalo Moreira <goncaloram@hotmail.com>
2022-09-14 17:55:18 +0100

    Finished demo 3 of the series

--- exercise_matplotlib_demo3_barcharts.py
+++ exercise_matplotlib_demo3_barcharts.py
@@ -1,12 +1,13 @@
 from matplotlib import pyplot as plt
 import numpy as np
+from collections import Counter
 
 grades = np.array([83, 95,91, 87, 70, 0, 85, 82, 100, 67, 73, 77, 0])
 decile = lambda grade: grade // 10*10
 histogram = Counter(decile(grade) for grade in grades)
 
 plt.bar([x - 4 for x in histogram.keys()],
-        histogram.valuess(),
+        histogram.values(),
         8)
 
 plt.axis([-5,105,0,5])
